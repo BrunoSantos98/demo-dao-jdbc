0ccd72bdb0a04812814474e942b0a3844405e60b
bruno santos <bruno4t@hotmail.com>
2022-11-15 09:55:52 -0300

    Criação da classe department

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,9 @@
+package application;
+
+import model.entities.Department;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
     }
 }
